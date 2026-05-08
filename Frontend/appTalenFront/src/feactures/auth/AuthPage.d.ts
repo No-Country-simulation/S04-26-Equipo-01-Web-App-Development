eb41a35077e351317566902306a9cd5baa340fac
Brian Desktop <briangelhorn@gmail.com>
2026-05-08 16:46:13 -0300

    Se declaró el tipo para evitar problemas de lint

--- Frontend/appTalenFront/src/feactures/auth/AuthPage.d.ts
+++ Frontend/appTalenFront/src/feactures/auth/AuthPage.d.ts
@@ -1,1 +1,7 @@
-export declare const AuthPage: React.FC<{ onLoginSuccess: (user: any) => void }>;
+import type { FC } from 'react';
+import type { AuthUser } from '../../App';
+
+export declare const AuthPage: FC<{
+  onLoginSuccess: (user: AuthUser) => void;
+  tab?: number;
+}>;
